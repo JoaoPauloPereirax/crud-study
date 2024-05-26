3bf0617ded11897b8f0ddaa43fa77cd790c0cf29
João Paulo Pereira <joaopaulop.dev@gmail.com>
2024-05-26 17:00:35 -0300

    Add to create function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Post;//Este item teve de ser adicionado, corrigindo o bug
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $request ){
+        $new_post = [
+            'title'=> 'Minha primeira postagem',
+            'content' => 'Algum texto',
+            'author' => 'João Paulo'
+        ];
+
+        //FORMA MAIS CONVENCIONAL DE CRIAR UM RESGISTRO NO BANCO
+        /*
+        $post = new Post($new_post);
+        $post->save();
+        */
+        $post = new Post();
+        $post->title = 'Minha segunda postagem';
+        $post->content = 'Algum texto';
+        $post->author = 'Professor';
+        $post->save();
+        dd($post);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/posts/create',[PostController::class,'create']);
 
 Route::get('/', function () {
     return view('welcome');
